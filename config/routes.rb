9c3e0a64cfccfe6299a7468cacf9b68f8d14af30
Carlos Bugueño <carlos.bugueno.r@gmail.com>
2024-08-01 15:12:31 -0400

    authentication permissions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
