92ba0e9b8ac0a08eb77494aaec6d241c601c41ce
Carlos Bugueño <carlos.bugueno.r@gmail.com>
2024-08-01 23:52:15 -0400

    adding a navbar and js to dropdown.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,10 @@
 import { Application } from "@hotwired/stimulus"
 
-const application = Application.start()
+const application = Application.start();
+
+// Import and register all TailwindCSS Components or just the ones you need
+import { Dropdown } from "tailwindcss-stimulus-components"
+application.register('dropdown', Dropdown)
 
 // Configure Stimulus development experience
 application.debug = false
